23771fcb9ce682814c03513d5f9dd9db7bf64944
Thibault BRONCHAIN <thibault@bronchain.me>
2015-09-17 12:21:34 +0800

    big fix setup osx

--- client-osx/ss-client-osx.sh
+++ client-osx/ss-client-osx.sh
@@ -10,7 +10,11 @@ read -r PW
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install ss
+brew install python
 brew install shadowsocks-libev
+
+# install rumps
+sudo pip install rumps
 
 cat > /usr/local/etc/shadowsocks-libev.json <<EOF
 {
@@ -27,9 +31,9 @@ mkdir -p /usr/local/opt/
 cp SimpleShadowSocks.py /usr/local/opt/SimpleShadowSocks.py
 
 mkdir -p ~/Library/LaunchAgents/
-cp net.almaritech.simpleshadowsocks ~/Library/LaunchAgents/net.almaritech.simpleshadowsocks
+cp com.almaritech.simpleshadowsocks.plist ~/Library/LaunchAgents/com.almaritech.simpleshadowsocks.plist
 
-launchctl load ~/Library/LaunchAgents/net.almaritech.simpleshadowsocks
-launchctl start net.almaritech.simpleshadowsocks
+launchctl load ${HOME}/Library/LaunchAgents/com.almaritech.simpleshadowsocks.plist
+launchctl start com.almaritech.simpleshadowsocks.plist
 
 echo "Done."
